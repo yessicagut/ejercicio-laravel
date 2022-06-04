578a0eeb72e578f4aa2198f6a1dd16a3e848ad91
Yessica Gutierrez Tamayo <yessica.gutierrez@ucp.edu.co>
2022-06-03 21:25:39 -0500

    iniciando proyecto

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('saludar/{nombre}',function(){
+    return view ('home',['nombre'=> $nombre]);
+});
